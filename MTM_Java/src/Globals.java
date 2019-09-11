2a6f40abefce5418c1267a8d6036afa497561293
2019teamwork <54949573+2019teamwork@users.noreply.github.com>
2019-09-11 14:17:14 -0400

    Delete Globals.java

--- MTM_Java/src/Globals.java
+++
@@ -1,8 +0,0 @@
-
-import java.io.File;
-
-public class Globals {
-	public static final File projFolder = new File(System.getProperty("user.dir"));// Project folder;
-	public static final String userFolder = System.getProperty("user.home");
-
-}
